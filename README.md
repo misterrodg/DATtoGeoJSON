e2bd1d4a0a204952bd5ea9260b84830b68bf3a6b
Kyle Rodgers <65290193+misterrodg@users.noreply.github.com>
2022-12-29 15:41:21 -0500

    Initial Build

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # DATtoGeoJSON
-DAT file to GeoJSON Converter
+
+A simple Python-based FAA DAT to GeoJSON Converter.
+
+## Limitations
+
+The DAT files that I was provided assume that coordinates are N Lat and W Lon, so the W coordinates are positive. This will not work for facilities at/near the antimeridian until I can see how that case is handled in DAT files from that area.
+
+## Requirements
+
+Python3 (Tested with Python 3.9.13)
+
+Only imports are OS and Math, so it should run on a standard Python implementation.
+
+## Instructions for use
+
+To process files for a single facility:
+
+1. Place the DAT files in the `dat_source` directory.
+2. Run the following command:
+   <br/>
+   ```
+   python3 convert.py [--radius X (default 140)]
+   ```
+
+To process multiple facilities at once:
+
+1. Create folders for each facility in the `dat_source` directory.
+2. Place the DAT files for each facility in their respective facility folders.
+   <br/>**Note:** the script will use the file names as the export name (e.g. `/dat_source/IAD/Base.dat` will be saved as `/output/IAD_Base.geojson`)
+   <br/><img src="./resources/facilities.jpg" alt="Folder Structure" width="200"/>
+3. Run the following command:
+   <br/>
+   ```
+   python3 convert.py [--radius X (default 140)]
+   ```
+
+### Optional Command Line Arguments
+
+The optional `--radius X` argument is provided because the DAT files include everything that is also shown in the PDF accompanying the DAT file, to include the border and text at the bottom left and right. This border is generally around 150NM from the center point (point of tangency) declared in the file, and there is a radial line showing MAG versus TRUE that extends inside of the border, necessitating a radius smaller than the border itself. The radius may be smaller for smaller TRACONS or Towers, so the default of 140 can be overridden by passing `--radius X`, where `XX` is an integer representing the NM from the center of the map. Example: `python3 convert.py --radius 50`
+<br/>
+
+<p align="center">
+<img src="./resources/border.jpg" alt="Folder Structure" width="400"/>
+</p>
+
+# Contributions
+
+Want to contribute? Check out the [Open Issues](https://github.com/misterrodg/DATtoGeoJSON/issues), or fork and open a [Pull Request](https://github.com/misterrodg/DATtoGeoJSON/pulls).
+
+Additional contributors will be listed here.
+
+# License
+
+This is licensed under GPL 3.0.
---
+++ convert.py
@@ -0,0 +1,189 @@
+import os
+import math
+import argparse
+
+
+class FileHandler:
+    def __init__(self):
+        self.localPath = os.getcwd()
+
+    def checkDir(self, subdirPath):
+        result = False
+        dirPath = self.localPath + "/" + subdirPath
+        os.makedirs(name=dirPath, exist_ok=True)
+        if os.path.exists(dirPath):
+            result = True
+        return result
+
+    def deleteAllInSubdir(self, fileType, subdirPath=None):
+        # As it stands, this will only ever delete items in the named subfolder where this script runs.
+        # Altering this function could cause it to delete the entire contents of other folders where you wouldn't want it to.
+        # Alter this at your own risk.
+        if subdirPath != None:
+            deletePath = self.localPath + "/" + subdirPath
+            for f in os.listdir(deletePath):
+                if f.endswith(fileType):
+                    os.remove(os.path.join(deletePath, f))
+
+    def searchForType(self, fileType, subdirPath=None):
+        result = []
+        searchPath = self.localPath
+        if subdirPath != None:
+            searchPath += "/" + subdirPath
+        for (dirpath, subdirs, files) in os.walk(searchPath):
+            result.extend(os.path.join(dirpath, f)
+                          for f in files if f.endswith(fileType))
+        return result
+
+    def splitFolderFile(self, fullPath, subdirPath=None):
+        result = []
+        split = os.path.split(fullPath)
+        searchPath = self.localPath
+        if subdirPath != None:
+            searchPath += "/" + subdirPath
+        result.append(split[0].replace(searchPath, ''))
+        result.append(split[1])
+        return result
+
+
+class Coordinate:
+    DEG_TO_MIN = 60
+
+    def __init__(self, lat=0.0, lon=0.0):
+        self.lat = lat
+        self.lon = lon
+
+    def fromDMS(self, latD, latM, latS, lonD, lonM, lonS):
+        self.lat = latD + (latM / 60) + (latS / (60*60))
+        self.lon = -lonD - (lonM / 60) - (lonS / (60*60))
+
+    def haversineGreatCicleDistance(self, lat, lon):
+        theta = self.lon - lon
+        arc = math.degrees(math.acos((math.sin(math.radians(self.lat)) * math.sin(math.radians(lat))) + (
+            math.cos(math.radians(self.lat)) * math.cos(math.radians(lat)) * math.cos(math.radians(theta)))))
+        distance = arc * self.DEG_TO_MIN
+        return distance
+
+    def toString(self):
+        return "[" + str(self.lon) + ", " + str(self.lat) + "]"
+
+
+class GeoJSON:
+    def __init__(self):
+        self.type = "FeatureCollection"
+        self.features = []
+
+    def addFeature(self, feature):
+        self.features.append(feature)
+
+    def toString(self):
+        features = ",\n\t\t".join(self.features)
+        return "{\n\t\"type\": \"FeatureCollection\",\n\t\"features\": [\n\t\t" + features + "\n\t]\n}"
+
+
+class LineString:
+    def __init__(self):
+        self.type = "Feature"
+        self.geometryType = "LineString"
+        self.coordinates = []
+
+    def addCoordinate(self, coordinate):
+        self.coordinates.append(coordinate)
+
+    def toString(self):
+        coords = ",\n\t\t\t\t\t".join(self.coordinates)
+        result = "{\n\t\t\t\"type\": \"Feature\",\n\t\t\t\"geometry\": {\n\t\t\t\t\"type\": \"LineString\",\n\t\t\t\t\"coordinates\": [\n\t\t\t\t\t" + \
+            coords + \
+            "\n\t\t\t\t]\n\t\t\t},\n\t\t\t\"properties\":{\n\t\t\t\t\"style\": \"solid\",\n\t\t\t\t\"thickness\":1\n\t\t\t}\n\t\t}"
+        self.coordinates.clear()
+        return result
+
+
+class DAT:
+    def __init__(self, sourceFolder, outputFolder, facFolder, fileName, limit=None):
+        self.fileName = "./" + sourceFolder + "/" + facFolder + "/" + fileName + ".dat"
+        folderPrefix = ""
+        if facFolder != "":
+            folderPrefix = facFolder + "_"
+        self.outputFileName = "./" + outputFolder + \
+            "/" + folderPrefix + fileName + ".geojson"
+        self.limit = limit
+        self.pot = None
+        self.read()
+
+    def lineToCoordinate(self, line):
+        latD = int(line[3:5])
+        latM = int(line[6:8])
+        latS = float(line[9:16])
+        lonD = int(line[18:21])
+        lonM = int(line[22:24])
+        lonS = float(line[25:32])
+        result = Coordinate()
+        result.fromDMS(latD, latM, latS, lonD, lonM, lonS)
+        if self.pot != None:
+            distance = result.haversineGreatCicleDistance(
+                self.pot.lat, self.pot.lon)
+            if self.limit != None:
+                if distance == 0 or distance > self.limit:
+                    result = -1
+        return result
+
+    def read(self):
+        datFile = open(self.fileName, "r")
+        geoJson = GeoJSON()
+        lineString = LineString()
+        for line in datFile:
+            if self.pot == None:
+                if line[:1] != "!" and line[:4] != "LINE":
+                    self.pot = self.lineToCoordinate(line)
+            else:
+                if line[:1] != "!" and line[:4] == "LINE":
+                    if len(lineString.coordinates) > 1:
+                        geoJson.addFeature(lineString.toString())
+                else:
+                    coordinate = self.lineToCoordinate(line)
+                    if coordinate != -1:
+                        lineString.addCoordinate(coordinate.toString())
+
+        data = geoJson.toString()
+        if os.path.exists(self.outputFileName):
+            os.remove(self.outputFileName)
+        resultFile = open(self.outputFileName, "w")
+        for line in data:
+            resultFile.write(line)
+        resultFile.close()
+        datFile.close()
+
+
+def main():
+    # Set up Argument Handling
+    parser = argparse.ArgumentParser(description="DATtoGeoJSON Converter")
+    parser.add_argument(
+        "--radius", type=int, help="The limit radius from the center point declared in the file.")
+    args = parser.parse_args()
+    radiusLimit = args.radius if args.radius != None else 140
+    print("Limiting to " + str(radiusLimit))
+    # Set up Script
+    fileHandler = FileHandler()
+    sourceDir = "dat_source"
+    outputDir = "output"
+    print("Initializing GeoJSON Converter")
+    fileHandler.checkDir(outputDir)
+    fileHandler.deleteAllInSubdir(".geojson", outputDir)
+    fileList = fileHandler.searchForType(".dat", sourceDir)
+    numFiles = str(len(fileList))
+    print("Found " + numFiles + " .dat files in ./" + sourceDir)
+    fileCount = 0
+    for f in fileList:
+        fileData = fileHandler.splitFolderFile(f, sourceDir)
+        folder = fileData[0]
+        fileName = fileData[1].replace(".dat", "")
+        print("[" + str(fileCount + 1) + "/" + numFiles + "] " +
+              "Processing " + fileName + ".dat")
+        DAT(sourceDir, outputDir, folder, fileName, radiusLimit)
+        fileCount += 1
+    print("***** Conversion complete. Files located in ./" + outputDir)
+
+
+if __name__ == "__main__":
+    main()
