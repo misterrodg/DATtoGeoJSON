979b7a4aaca818c2884153e88a63c7c8c3eef7f0
Kyle Rodgers <65290193+misterrodg@users.noreply.github.com>
2023-01-01 15:59:55 -0500

    Refactor GeoJSON Class

--- convert.py
+++ convert.py
@@ -2,17 +2,8 @@ import os
 import argparse
 
 
-class GeoJSON:
-    def __init__(self):
-        self.type = "FeatureCollection"
-        self.features = []
 
-    def addFeature(self, feature):
-        self.features.append(feature)
 
-    def toString(self):
-        features = ",\n\t\t".join(self.features)
-        return "{\n\t\"type\": \"FeatureCollection\",\n\t\"features\": [\n\t\t" + features + "\n\t]\n}"
 
 
 class LineString:
